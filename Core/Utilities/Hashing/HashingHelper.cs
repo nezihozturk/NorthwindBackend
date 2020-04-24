baca10cf1ac6971ebe32902cac7a9e803e5c11b8
Nezih Öztürk <nzhzturk@hotmail.com>
2020-04-24 14:42:58 +0200

    commit2

--- Core/Utilities/Hashing/HashingHelper.cs
+++ Core/Utilities/Hashing/HashingHelper.cs
@@ -14,6 +14,7 @@ namespace Core.Utilities.Hashing
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
                 //test
+                //test2
 
 
             }
